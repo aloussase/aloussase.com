1e14db8b033466670f42d79139f1cdcab36d1e5f
Alexander Goussas <goussasalexander@gmail.com>
2024-12-31 08:38:30 -0500

    improve SEO

--- src/Main.hs
+++ src/Main.hs
@@ -15,23 +15,35 @@ import qualified Text.Blaze.Html5.Attributes          as A
 import           Web.Scotty
 
 viewHead :: String -> Html
-viewHead title =
+viewHead title = H.html ! A.lang "en" $
   H.head $ do
     H.link ! A.rel "stylesheet" ! A.href "/styles.css"
     H.meta ! A.name "viewport" ! A.content "width=device-width, initial-scale=1"
+    H.meta ! A.name "description" ! A.content (mconcat
+        [ "Alexander is a software developer from Ecuador. He is interested in and writes regularly about "
+        , "functional programming and distributed systems in his blog. In his free time, he likes doing "
+        , "street photography, reading and working on side-projects. If you share his love for FP and Haskell "
+        , "or programming in general, don't hesitate to reach out to him."
+        ])
+    H.meta ! A.name "author" ! A.content "Alexander Goussas"
+    H.meta ! A.name "keywords" ! A.content "Haskell, Blog, Programming"
     H.title $ H.toHtml title
 
 viewHome :: Html
 viewHome = viewHead "Home | Alexander Goussas" >> viewAppBar >> H.main (do
   H.h1 "Alexander Goussas"
-  H.img ! A.src "/me.jpg"
-  H.div "Hello there! I am Alexander, a programmer from the city of Guayaquil, Ecuador."
-  H.div $ mconcat
-    ["I am currently finishing my Computer Engineering degree while also working as a software developer"
-    , " at a retail company in my city."
+  H.img ! A.alt "Haskell programmer" ! A.src "/me.jpg"
+  H.p $ mconcat
+    [ "Hello there! I am Alexander, a programmer from the city of Guayaquil, Ecuador. "
+    , "I am currently finishing my Computer Engineering degree while also working as a software developer "
+    , "at a respectable software company TM."
     ]
-  H.div $ mconcat
-    ["I run an online programming book club (see below if you are interested) and love talking all things programming,"
+  H.p $ mconcat 
+    [ "Among other things, I love photography, tattoos and learning foreign languages. "
+    , "I do street photography on my free time and I'm currently learning French. "
+    ]
+  H.p $ mconcat
+    ["Lastly, I run an online programming book club (see below if you are interested) and love talking all things programming,"
     ," so don't hesitate to reach out if you want to chat!"]
   viewInterestingStuff
   viewSocials)
@@ -90,6 +102,7 @@ viewInterestingStuff = do
     H.li $ do
       "My blog "
       H.a ! A.href "https://aloussase.substack.com" $ "Ramblings of the One-Eyed Daemon"
+      " where I talk mostly about web stuff, distributed systems and Haskell"
     H.li $ do
       "The online book club I run "
       H.a ! A.href "/spbc" $ "GYE Systems Programming Book Club"
@@ -97,7 +110,7 @@ viewInterestingStuff = do
       "The Discord server I run "
       H.a ! A.href "https://discord.gg/kbK45MDk7q" $ "Invite link"
     H.li $ do
-      "Co-hosted YouTube channel talking all things FP "
+      "Co-hosted YouTube channel talking all things functional programming "
       H.a ! A.href "/curry-corner" $ "The Curry Corner"
     H.li $ do
       "Free Software university club (I was president for a couple years) "
@@ -106,7 +119,7 @@ viewInterestingStuff = do
 viewSocials :: Html
 viewSocials = do
   H.h2 "Socials"
-  H.div "I don't really use social media. The best way to reach me is email or LinkedIn"
+  H.div "I don't really use social media. The best way to reach me is either by email or LinkedIn"
   H.ul $ do
     H.li $ H.a ! A.href "https://linkedin.com/in/alexander-goussas" $ "LinkedIn"
     H.li $ H.a ! A.href "https://github.com/aloussase" $ "GitHub"
